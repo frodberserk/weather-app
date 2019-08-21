eec14c746161d0dcb404f973970846f8b0ba155b
Ankit Garg <er.ankitgrg@gmail.com>
2019-08-21 13:50:19 +0530

    Added test case to test getCityWeatherRequest

--- app/src/test/java/com/frodberserk/weatherapp/WeatherApiImageTest.kt
+++ app/src/test/java/com/frodberserk/weatherapp/WeatherApiImageTest.kt
@@ -1,17 +1,33 @@
 package com.frodberserk.weatherapp
 
+import com.frodberserk.weatherapp.web.WeatherApi
 import com.frodberserk.weatherapp.web.WeatherImageUtil
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.net.URLDecoder
 
 @RunWith(JUnit4::class)
 class WeatherApiImageTest {
 
     @Test
     fun test_getWeatherImageUrl() {
-        print(WeatherImageUtil.getWeatherImageUrl("01n"))
-        assert(WeatherImageUtil.getWeatherImageUrl("01n").equals("http://openweathermap.org/img/wn/01n@2x.png"))
+        WeatherImageUtil.getWeatherImageUrl("01n").let {
+            println(it)
+            assert(it == "http://openweathermap.org/img/wn/01n@2x.png")
+        }
+    }
+
+    @Test
+    fun test_getCityWeatherRequest_url() {
+        URLDecoder.decode(
+            WeatherApi.getCityWeatherRequest("Sangrur", "India").url.toString(),
+            Charsets.UTF_8.toString()
+        ).let {
+            println(it)
+            assert(it == "http://api.openweathermap.org/data/2.5/weather?appid=ac94b4a082858e273dcd5236fc9694f1&q=Sangrur,India")
+        }
+
     }
 
 }
